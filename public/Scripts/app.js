7a631aa64f742331c3744b0fe0b5a24b6730b0bc
AbhishekMalhotra <amalho26@my.centennialcollege.ca>
2022-06-18 23:03:20 -0400

    First Commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,13 @@
+/* FileName :app.js */
+/* StudentName :Abhishake Malhotra */
+/* Student Id :301177229 */
+(function(){
+
+
+    function Start(){
+        console.log("App Started.....");
+    }
+
+    window.addEventListener("load",Start);
+
+})();
---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports={
+"URI": "mongodb+srv://admin:IHOe0oESgBLyVZEQ@portfoliosite.x9cg7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+}
